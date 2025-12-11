d48f2e8d4c9ffe7fa25cbc93840ff8313be1c274
thrid3v <thridev36@gmail.com>
2025-12-11 19:22:44 +0530

    Time: 101 ms (42.47%) | Memory: 62 MB (53.56%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(), nums.end());
         int operations = 0;
         int left = 0;
-        int right = nums.size()-1;
+        int right = nums.size() - 1;
         while (left < right) {
             int sum = nums[left] + nums[right];
             if (sum == k) {
